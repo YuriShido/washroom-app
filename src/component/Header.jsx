d98811e7d01f2d6146e962b298a6401196a52ef4
YuriShido <sydonce57@gmail.com>
2021-07-30 10:52:54 -0700

    add header

---
+++ src/component/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import "../App.scss"
+import "./Header.scss"
+import About from './About'
+
+const Header = () => {
+    const [AboutModalOpen, setAboutModalOpen] = React.useState(false);
+
+    // const handleAboutModal = () => {
+    //     setAboutModalOpen= true
+    // }
+    return (
+        <div className="header-container">
+            <img className="top-logo" src="/img/logo3.png" alt="logo"/>
+            {/* <h1>Run Van W/C</h1> */}
+            <p className="about" onClick={() => setAboutModalOpen(true)}>About</p>
+            {
+                AboutModalOpen ? (
+                    <About AboutModalOpen={AboutModalOpen} setAboutModalOpen={setAboutModalOpen}/>
+                )  : null
+            }
+        </div>
+    )
+}
+
+export default Header
